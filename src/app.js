c73acee3cedd0a7a21f0f8f8601e30cd5f87d64a
JohnGabriel <john.gabriel@aluno.ifsp.edu.br>
2022-07-06 14:37:36 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,10 @@ class App {
       express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
     );
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header(
+        "Access-Control-Allow-Origin",
+        "https://e-commerce-eight-amber.vercel.app"
+      );
       res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.header(
