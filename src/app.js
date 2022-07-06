55041985331f26b069c94ebc7170b218076f6cd8
JohnGabriel <john.gabriel@aluno.ifsp.edu.br>
2022-07-06 09:54:37 -0300

    Atualizando para o heroku

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ class App {
       express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
     );
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
       res.header(
         "Access-Control-Allow-Headers",
