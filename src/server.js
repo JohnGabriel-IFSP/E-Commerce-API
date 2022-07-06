c6eb11c28dc9e3e82d597cc9a15d0052c812de01
JohnGabriel <john.gabriel@aluno.ifsp.edu.br>
2022-07-06 10:13:06 -0300

    Correção server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,7 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 8080;
-const porta = process.env.PORT || 8000;
-const ip = require('ip').address();
-const protocol = process.env.PROTOCOL || 'http'
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
 });
-
-
-app.listen(porta, ()=>{
-  console.log(`Server inicou em http://localhost:${porta} or ${protocol}://${ip}:${porta}`)
-})
